73423910c67bbf9d89aee38f4f9ab0da5fa0001a
Andrei <andrei.cristea997@gmail.com>
2019-11-08 20:32:13 +0100

    Realised Small Website

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// A $( document ).ready() block.
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navClass: ["slider__nav-left", "slider__nav-right"],
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      900: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      }
+    }
+  })
+});
